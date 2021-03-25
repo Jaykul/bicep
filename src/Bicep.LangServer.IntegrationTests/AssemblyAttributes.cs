48a3d104cd17535f3516538ca3b2a6c8273b70b2
Marcin Jastrzebski <majastrz@microsoft.com>
2021-03-24 18:14:15 -0700

    try disabling parallelism in language server tests

--- src/Bicep.LangServer.IntegrationTests/AssemblyAttributes.cs
+++ src/Bicep.LangServer.IntegrationTests/AssemblyAttributes.cs
@@ -2,5 +2,5 @@
 // Licensed under the MIT License.
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// reduce parallelism for language server integration tests
-[assembly: Parallelize(Scope = ExecutionScope.ClassLevel, Workers = 0)]
+// disable parallelism for language server integration tests
+[assembly: Parallelize(Scope = ExecutionScope.ClassLevel, Workers = 1)]
